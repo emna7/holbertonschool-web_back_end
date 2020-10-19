fd3bf57960238bf9293c92dfaaff68748d411f57
emna7 <bhmemna7@gmail.com>
2020-10-19 16:47:39 +0100

    create

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
+"""
+to string
+"""
+
+
 def to_str(n: float) -> str:
+    """string representation of floor
+    """
     return str(n)
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+list of floats
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """return sum as float
+    """
+    sum: float = 0
+    for i in input_list:
+        sum = sum +i
+    return float
