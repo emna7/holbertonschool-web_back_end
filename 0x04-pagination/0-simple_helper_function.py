e1fa0056a82718abe872d67252b6adb7dc6832de
emna7 <bhmemna7@gmail.com>
2020-10-28 14:05:11 +0100

    create

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+simple helper function
+"""
+
+
+from typing import Tuple
+
+
+def index_range(page:int, page_size: int) -> Tuple[int, int]:
+    """indexes
+    """
+    return (0,7)
