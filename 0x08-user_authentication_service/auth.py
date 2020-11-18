6d4c1364cb5bceb3c823d0c949b50e1e015da8b5
emna7 <bhmemna7@gmail.com>
2020-11-18 19:48:13 +0100

    create

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -8,6 +8,8 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
+from typing import Union
 
 
 def _hash_password(password: str) -> str:
