24de4a76840162cf518c96c82cc717d3132bb517
emna7 <bhmemna7@gmail.com>
2020-11-18 19:46:23 +0100

    create

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -1,23 +1,18 @@
-#!/sur/bin/env python3
-"""
-User model
-"""
-
+#!/usr/bin/env python3
+"""Define User model"""
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
 
 Base = declarative_base()
 
 
 class User(Base):
-    """declare usuer
-    """
+    """Represent a user"""
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
