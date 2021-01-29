7a0fdf4deee2bead94e7752d05a5234417526e64
emna7 <bhmemna7@gmail.com>
2021-01-29 12:18:35 +0100

    create

--- 0x13-unittests_in_js/utils.js
+++ 0x13-unittests_in_js/utils.js
@@ -1,24 +1,16 @@
-const Utils {
-    calculateNumber(type, a, b) {
-    const numA = Number(a);
-    const numB = Number(b);
-
-    if (Number.isNaN(numA) || Number.isNaN(numB))
-    throw TypeError;
-
-    if (type === 'SUM') {
-      return (Math.round(numA) + Math.round(numB));
-    } else if (type === 'SUBTRACT') {
-      return (Math.round(numA) - Math.round(numB));
-    } else if (type === 'DIVIDE') {
-        if (Math.round(numB) === 0) {
-          return ('Error');
-        }
-        return (Math.round(numA) / Math.round(numB));
-    } else {
-      throw TypeError;
-    }
-  }
+let Utils = {
+	calculateNumber: (type, a, b) => {
+		if (type === 'SUM') {
+			return (Math.round(a) + Math.round(b));
+		} else if (type === 'SUBTRACT') {
+			return (Math.round(a) - Math.round(b));
+		} else if (type === 'DIVIDE') {
+			if (Math.round(b) === 0) {
+				return 'Error';
+			}
+			return (Math.round(a) / Math.round(b));
+		}
+	}
 };
 
 module.exports = Utils;
