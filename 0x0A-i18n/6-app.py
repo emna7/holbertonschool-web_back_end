985d1bb7a00216107227ce132f44986eea93fd8d
emna7 <bhmemna7@gmail.com>
2021-02-03 18:51:58 +0100

    create

--- 0x0A-i18n/6-app.py
+++ 0x0A-i18n/6-app.py
@@ -1,24 +1,17 @@
 #!/usr/bin/env python3
-"""Basic babel setup
-"""
-
-
+""" flask app"""
 from flask import Flask, render_template, request, g
-from flask_babel import Babel, gettext
-from typing import Optional
+from flask_babel import Babel, _
+
+app = Flask(__name__)
 
 
-class Config:
-    """configuration
-    """
+class Config(object):
+    """ Config class """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
-app = Flask(__name__)
-app.config.from_object(Config)
-babel = Babel(app)
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -26,45 +19,46 @@ users = {
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
-
-
-def get_user(login_as):
-    """look for a user and get it
-    """
-    if login_as is None:
-        return None
-    u = users.get(login_as)
-    if u:
-        return u
-    return None
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.before_request
 def before_request():
-    """set it global
-    """
-    login_id = request.args.get('login_as')
-    if login_id:
-        search_user = get_user(int(login_id))
-        g.user = search_user
+    """determine if a user is logged in"""
+    if (request.args.get('login_as')):
+        id = int(request.args.get('login_as'))
+        user = get_user(id)
+        if user:
+            g.user = user
 
 
-@babel.localeselector
-def get_local() -> Optional[str]:
-    """get locale time
-    """
-    locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
-        return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+def get_user(id):
+    """get user"""
+    if id in users:
+        return users[id]
+    return None
 
 
 @app.route('/')
-def index():
-    """call index template
-    """
-    return render_template('6-index.html')
+def hello():
+    """ render html file """
+    login = False
+    if g.get('user') is not None:
+        login = True
+    return render_template('6-index.html', login=login)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+@babel.localeselector
+def get_locale():
+    """ best match with the supported languages """
+    x = request.args.get('locale')
+    if x in app.config['LANGUAGES']:
+        return x
+    if (g.get('user') and g.user["locale"] in app.config['LANGUAGES']):
+        return g.user["locale"]
+    return(request.accept_languages.best_match(app.config['LANGUAGES']))
+
+
+if __name__ == '__main__':
+    app.run()
